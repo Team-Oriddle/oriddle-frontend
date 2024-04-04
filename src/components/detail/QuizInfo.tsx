ff0795bd6a3be90d547fae5149a80ed99e51abf5
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-04-04 14:43:11 +0900

    feat : 퀴즈 정보 페이지 API 연동

--- src/components/detail/QuizInfo.tsx
+++ src/components/detail/QuizInfo.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components"
 import DefalutButton from "../common/DefalutButton"
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { useSearchParams } from "next/navigation"
 
 
 const Layout = styled.div`
@@ -34,14 +37,44 @@ const QuizDetail = styled.div`
   margin: 30px 65px;
 `
 
-export const QuizInfo = () =>{
+interface Props{
+  quizId :number
+}
+
+export const QuizInfo = ({quizId}:Props) =>{
   const ment = '게임 시작' 
+  const id = quizId
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  //추후에 구조 분해 할당 활용한 코드로 수정
+
+  useEffect(()=>{
+    const fetchQuizDetail =async (id:number) => {
+      try {
+        const response = await axios.get(`http://localhost:8080/api/v1/quiz/info/${id}`)
+
+        console.log(response)
+        setTitle(response.data.data.title)
+        setDescription(response.data.data.description)
+        
+      } catch (error) {
+        console.error('Error fetching quiz info:', error);
+      }
+    }
+
+    fetchQuizDetail(id)
+  })
+
   return (
     <Layout>  
       <QuizImg>썸네일 이미지</QuizImg>
       <QuizDetailLayout>
         <QuizDetail>
+          {title}
+          {description}
           <DefalutButton color="보라" txt={ment}></DefalutButton>
+          
         </QuizDetail>
       </QuizDetailLayout>
     </Layout>
