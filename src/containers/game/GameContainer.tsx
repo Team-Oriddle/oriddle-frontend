99d90db32646252cebd0a79d85ad5185cd24462a
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-04-06 16:26:04 +0900

    feat : 퀴즈 진행 페이지 API 임시 연동

--- src/containers/game/GameContainer.tsx
+++ src/containers/game/GameContainer.tsx
@@ -4,6 +4,10 @@ import { Question } from "@/components/game/Question";
 import { QuizSource } from "@/components/game/QuizSource";
 import { Header } from "@/components/header/Header";
 import styled from "styled-components";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import InGameSocket from "@/socket/InGameChatSocket";
 
 const Container = styled.div`
   display: flex;
@@ -25,7 +29,69 @@ const Wrapper = styled.div`
 `
 
 
-const GameContainer: React.FC = () => {
+const GameContainer: React.FC = ({quizId}:Props) => {
+
+  const quizRoomId = quizId 
+
+  const navigate = useRouter();
+  
+  const [isLoading, setIsLoading] = useState(true);
+  const [userData, setUserData] = useState([]);
+  const [modalOpen, setModalOpen] = useState(true);
+  const [questionData, setQuestionData] = useState({
+    number: 0,
+    description: "퀴즈가 준비중입니다",
+    type: "미정",
+    sourceType: "IMAGE",
+    source: null,
+    score: "미정",
+    timeLimit: 30
+  });
+  const [gameSocket, setGameSocket] = useState(null);
+  const [countdown, setCountdown] = useState(5);
+
+  const getUserData = useCallback(async () => {
+    try {
+      const response = await axios.get(`http://localhost:8080/api/v1/quiz-room/${quizRoomId}`, {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+      console.log(response);
+      setUserData(response.data.data.participants);
+      const socket = new InGameSocket(quizRoomId, setParticipants, setQuestionData, setAnswer, navigate);
+      setGameSocket(newGameSocket);
+      setQuestionData(newGameSocket.QuestionData);
+      setIsLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  }, [quizRoomId, navigate, userData, questionData]);
+
+  useEffect(() => {
+    let intervalId = null;
+    if (gameSocket && gameSocket.answer) {
+      setCountdown(5);
+      intervalId = setInterval(() => {
+        setCountdown(prevCountdown => {
+          const newCountdown = prevCountdown - 1;
+          if (newCountdown <= 0) {
+            clearInterval(intervalId);
+          }
+          return newCountdown;
+        });
+      }, 1000);
+    }
+    return () => clearInterval(intervalId);
+  }, [gameSocket]);
+
+  useEffect(() => {
+    getUserData();
+  }, [getUserData]);
+
+
+
 
   return (
     <Container>
