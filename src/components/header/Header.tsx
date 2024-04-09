c36ad58f5b47f6c8e4c697207d255f1c9753d27c
TMInstaller <bjdy3000@gmail.com>
2024-04-09 21:51:13 +0900

    feat: header 디자인 수정, url 링크

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,5 @@
-import styled from "styled-components"
+import { useRouter } from "next/navigation";
+import styled from "styled-components";
 
 const Layout = styled.div`
   padding-top: 30px;
@@ -9,45 +10,61 @@ const Layout = styled.div`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
-  color: black
-`
+  color: black;
+`;
 
-const Title = styled.div`
+const Title = styled.h1`
+  all: unset;
   font-size: 40px;
   font-weight: bolder;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+  cursor: pointer;
+`;
+
 const Buttons = styled.div`
   width: 220px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-`
+  cursor: pointer;
+`;
 
-const Button = styled.div`
+const Button = styled.button`
+  all: unset;
+  color: black;
   width: 98px;
   height: 70px;
   font-size: 20px;
   display: flex;
   justify-content: center;
   align-items: center;
-`
+`;
 
-export const Header = () =>{
+export const Header = () => {
+  const router = useRouter();
+  const routeToMain = () => {
+    router.push("/");
+  };
 
-  return(
+  const routeToMyPage = () => {
+    alert("My Page로 이동합니다.");
+  };
+
+  const routeToLogin = () => {
+    alert("Login으로 이동합니다.");
+  };
+
+  return (
     <Layout>
-      <Title>ORIDDLE</Title>
+      <Title onClick={routeToMain}>ORIDDLE</Title>
+      {/* TODO: 검색 바 넣기 */}
       <Buttons>
-        <Button>
-          MY PAGE
-        </Button>
-        <Button>
-          LOGIN
-        </Button>
+        {/* TODO: 로그인 이전, 이후의 헤더 항목 업데이트 필요 */}
+        <Button onClick={routeToMyPage}>MY PAGE</Button>
+        <Button onClick={routeToLogin}>LOGIN</Button>
       </Buttons>
     </Layout>
-  )
-}
+  );
+};
