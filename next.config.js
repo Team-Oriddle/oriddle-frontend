e30609c6bc989a5be4f9d177a0c7fa22e08a9d93
TMInstaller <bjdy3000@gmail.com>
2024-04-10 23:20:07 +0900

    chore: s3 설정 추가

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["source.unsplash.com"],
+    domains: [
+      "source.unsplash.com",
+      "test-oriddle-bucket.s3.ap-northeast-2.amazonaws.com",
+    ],
   },
 };
 
