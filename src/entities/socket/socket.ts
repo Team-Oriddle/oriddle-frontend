c232290ba6689b6a9611867cc273035037652105
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-04-11 15:31:16 +0900

    refactor : socket entities 수정

--- src/entities/socket/socket.ts
+++ src/entities/socket/socket.ts
@@ -1,39 +1,27 @@
 // socket.ts
-import { Client } from 'stompjs';
+import { Client } from '@stomp/stompjs';
 
 let stompClient: Client | null = null;
 
-export const initializeSocket = (url: string) => {
+export const initializeSocket = (url: string, subscriptions: Array<{topic: string, callback: (message: any) => void}>) => {
   stompClient = new Client({
-    brokerURL: url,// 연결을 진행항 url
-    // connectHeaders: {
-    //   login: 'user',
-    //   passcode: 'password',
-    // }, 로그인 관련 부분 관련 X
-    // debug: function (str) {
-    //   console.log('STOMP: ' + str);
-    // }, 디버깅 설정
-    // reconnectDelay: 5000,
-    // heartbeatIncoming: 4000,
-    // heartbeatOutgoing: 4000,
+    brokerURL: url,
+    onConnect: () => {
+      console.log('서버 연결');
+      //주어진 리스트를 모두 구독
+      subscriptions.forEach(({ topic, callback }) => {
+        stompClient?.subscribe(topic, (message) => {
+          callback(JSON.parse(message.body));
+        });
+      });
+    },
+    onStompError: (frame) => {
+      console.error('Broker reported error: ' + frame.headers['message']);
+      console.error('Additional details: ' + frame.body);
+    },
   });
-  console.log('소켓 생성')
+
   stompClient.activate();
-};
-
-export const subscribeToMessages = (topic: string, callback: (message: any) => void) => {
-  if (!stompClient) {
-    console.error('STOMP client is not initialized.');
-    return;
-  }
-
-  stompClient.onConnect = () => {
-    stompClient?.subscribe(topic, (message) => {
-      if (message.body) {
-        callback(JSON.parse(message.body));
-      }
-    });
-  };
 };
 
 export const sendMessage = (destination: string, message: any) => {
@@ -42,5 +30,8 @@ export const sendMessage = (destination: string, message: any) => {
     return;
   }
 
-  stompClient.publish({ destination, body: JSON.stringify(message) });
+  stompClient.publish({
+    destination,
+    body: JSON.stringify(message),
+  });
 };
