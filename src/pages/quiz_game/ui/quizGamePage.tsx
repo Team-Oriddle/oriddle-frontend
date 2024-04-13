1bd219612261d9ff3dc743beef1d7aadd912250a
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-04-13 15:24:22 +0900

    refactor : quiz_game page 추가

---
+++ src/pages/quiz_game/ui/quizGamePage.tsx
@@ -0,0 +1,201 @@
+import { ChatInput } from "@/components/game/ChatInput";
+import { UserChat } from "@/components/game/UserChat";
+import { Question } from "@/components/game/Question";
+import { QuizSource } from "@/components/game/QuizSource";
+import { Header } from "@/components/header/Header";
+import styled from "styled-components";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import getQuizRoomData from "@/entities/quizroom/api/getQuizRoomData";
+import { initializeSocket } from "@/entities/socket/socket";
+import Modal from "@/components/game/Modal";
+import { SendMessage } from "@/features/SendMessage/ui/sendMessage";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  min-height: 100vh; // Viewport Height
+  width: 100vw; // Viewport Width
+  min-width: 100vw;
+  background-color: white;
+  color: black;
+`
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  display: grid;
+  grid-template-columns: repeat(12, 1fr);
+  column-gap: 24px;
+`
+
+type Props = {
+  quizId :number
+}
+
+interface QuestionDataType{
+  description:string
+  number:number
+  score:number
+  source:string
+  sourceType:string
+  timeLimit: number
+  type: string
+}
+
+interface UserData{
+  nickname:string,
+  position: number,
+  userId:number,
+  isHost:boolean,
+  score:number
+}
+
+interface ChatType{
+  user:string,
+  chat:string
+}
+
+interface AnswerType{
+  userId?:number,
+  answer:string,
+  score?:number,
+}
+
+
+const QuizGamePage = ({quizId}:Props) => {
+  const navigate = useRouter();
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [questionData, setQuestionData] = useState<QuestionDataType>({
+    description: "",
+    number: -1,
+    score: -1,
+    source: "",
+    sourceType: "",
+    timeLimit: -1,
+    type: "",
+  });
+  const [countdown, setCountdown] = useState(5);
+  const [chatLog, setChatLog] = useState<ChatType[]>([]);
+  const [userData, setUserData] = useState<UserData[]>([]);
+  const [answerData, setAnswerData] = useState<AnswerType>({
+    userId:-1,
+    answer:'',
+    score:-1,
+  });
+  const [ answerUser, setAnswerUser ] = useState<string>('')
+  const [answerModalOpen, setAnswerModalOpen] = useState(false);
+  const [finishModalOpen, setFinishModalOpen] = useState(false);
+
+  const toggleAnswerModal = () => setAnswerModalOpen(!answerModalOpen);
+  const toggleFinishModal = () => setFinishModalOpen(!finishModalOpen);
+
+
+
+  useEffect(()=>{
+    //TODO: API 호출부 변경
+    getQuizRoomData(quizId).then((result)=>{
+      console.log(result)
+      result.participants.forEach(participant =>{
+        participant.score = 0;
+      })
+      setUserData(result.participants)
+    })
+    //새로운 소켓 연결 해야함
+    setIsLoading(false)
+  },[quizId])
+
+
+  useEffect(()=>{
+    const getSocket = (quizRoomId:number) => {
+      const subscriptions = [
+        { topic: `/topic/quiz-room/${quizRoomId}/join`, callback:(message) =>{
+          let newUser = {...message, isHost:false}
+          setUserData([...userData,newUser])
+          userData.sort(function(a,b){
+            return a.position-b.position
+          })
+        }},
+        //strictMode가 켜져 있는 경우 제대로 작동하지 않음
+        { topic: `/topic/quiz-room/${quizRoomId}/leave`, callback:(message) =>{
+          const findIndex = message.userId;
+          const copyUserData = userData
+          const removeUserData = copyUserData.findIndex(player => player.userId === findIndex)
+          if (removeUserData !== -1) {
+            copyUserData.splice(removeUserData, 1);
+          }
+          setUserData(copyUserData);
+        }},
+        //strictMode가 켜져 있는 경우 제대로 작동하지 않음
+        { topic: `/topic/quiz-room/${quizRoomId}/question`, callback:(message) =>{
+          console.log(message)
+          setQuestionData(message)
+        }},
+        { topic: `/topic/quiz-room/${quizRoomId}/answer`, callback:(message) =>{
+          console.log(message)
+          setAnswerData(message)
+          let copyUserData = userData
+          let foundIndex = -1;
+          userData.forEach((participant, index)=>{
+            if(participant.userId === message.userId){
+              foundIndex = index
+            }
+          })
+          //사용자 닉네임 리턴 해줘야함
+          if(foundIndex !== -1){
+            copyUserData[foundIndex].score += message.score;
+            setAnswerUser(copyUserData[foundIndex].nickname)
+          }else{
+            console.log('사용자가 존재하지 않습니다!')
+          }
+          toggleAnswerModal()
+        }},
+        { topic: `/topic/quiz-room/${quizRoomId}/finish`, callback:(message) =>{
+          console.log(message)
+          //종료된 경우 결과 페이지로 이동
+        }},
+        { topic: `/topic/quiz-room/${quizRoomId}/time-out`, callback:(message) =>{
+          console.log(message)
+          setAnswerData(message)
+          toggleFinishModal()
+        }},
+        { topic: `/topic/quiz-room/${quizRoomId}/chat`, callback:(message) =>{
+          console.log(message)
+          //채팅 메시지 관리
+        }},
+      ]
+      initializeSocket('ws://localhost:8080/ws', subscriptions)//소켓 연결
+    }
+
+    getSocket(8)
+
+  },[isLoading])
+  ///처리해야할 데이터
+  //1. 타이머
+  //4. 유저 채팅 데이터
+
+  //chat의 경우 메세지가 오면 보여준다
+  return (
+    <Container>
+      <Wrapper>
+          <Header></Header>
+          <Question description={questionData.description} number={questionData.number} type={questionData.type} score={questionData.score} ></Question>
+          <QuizSource url={questionData.score} sourceType={questionData.sourceType}></QuizSource>
+          <UserChat UserList={userData} ></UserChat>
+          <SendMessage quizRoomId={quizId}></SendMessage>
+          <Modal isOpen={answerModalOpen} onClose={toggleAnswerModal}>
+            <div>정답:{answerData.answer}</div>
+            <div>{answerUser} 님이 정답을 맞추셨습니다</div>
+            <div> (+{answerData.score}점)</div>
+          </Modal>
+          <Modal isOpen={finishModalOpen} onClose={toggleFinishModal}>
+            <div>정답은 {answerData.answer}입니다</div>
+          </Modal>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default QuizGamePage;
