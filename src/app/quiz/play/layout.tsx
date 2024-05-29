cc01b95a041e66eda9535a49fe9794aa011ea788
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-05-29 13:02:09 +0900

    feat : 소켓 프로바이더 최상단으로 이동

--- src/app/quiz/play/layout.tsx
+++ src/app/quiz/play/layout.tsx
@@ -1,9 +1,24 @@
+'use client'
 import { SocketProvider } from "@/entities/socket/lib/SocketProvider";
+import { useState } from "react";
+
+interface UserData{
+  nickname:string,
+  position: number,
+  userId:number,
+  isHost:Boolean
+}
 
 export default function Layout({ children }) {
+
+  const [userData, setUserData] = useState<UserData[]>([]); 
+
+  //여기서 소켓 프로바이더로 넘겨줌
   return (
-    <SocketProvider>
+    <SocketProvider> 
       <main>{children}</main>
     </SocketProvider>
   )
 }
+
+//얘가 SPA 레이아웃임 ㅇ
