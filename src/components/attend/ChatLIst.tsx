425cf82b9e8ff54a3e5152478b5b9b0f27b3916e
JongYun Jeong <95991290+BellYun@users.noreply.github.com>
2024-06-06 15:59:54 +0900

    feat : 채팅창 구현

--- src/components/attend/ChatLIst.tsx
+++ src/components/attend/ChatLIst.tsx
@@ -1,5 +1,5 @@
-import styled from "styled-components"
-
+import { useEffect, useRef } from "react";
+import styled from "styled-components";
 
 const Layout = styled.div`
   width: 1074px;
@@ -10,14 +10,45 @@ const Layout = styled.div`
   filter: drop-shadow(0 0 10px rgba(0, 0, 0, 0.25));
   border-radius: 10px;
   margin-right: 12px;
+  color: black;
+  overflow-y: auto;
+  font-size: 22px;
+`;
 
-`
+const ChatMessage = styled.div`
+  padding: 10px;
+  display: flex;
+`;
 
-export const ChatList = () =>{
+const ChatNickname = styled.div`
+  font-weight: bold;
+  margin-bottom: 5px;
+`;
+
+const ChatContent = styled.div`
+  font-size: 22px;
+`;
+
+export const ChatList = ({ chatList }: any) => {
+
+  const chatEndRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (chatEndRef.current) {
+      chatEndRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+    console.log(chatList)
+  }, [chatList]);
 
   return (
     <Layout>
-      
+      {chatList.map((chat: any, index: number) => (
+        <ChatMessage key={index}>
+          <ChatNickname>{chat.nickname}:</ChatNickname>
+          <ChatContent>{chat.content}</ChatContent>
+        </ChatMessage>
+      ))}
+      <div ref={chatEndRef} />
     </Layout>
-  )
-}
+  );
+};
