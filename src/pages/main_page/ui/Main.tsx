9c0fd6daef5c480963412248660156dc6d99d85a
TMInstaller <bjdy3000@gmail.com>
2024-06-17 22:45:00 +0900

    feat: 배경 적용법 추가

--- src/pages/main_page/ui/Main.tsx
+++ src/pages/main_page/ui/Main.tsx
@@ -5,18 +5,19 @@ import { useEffect } from "react";
 import styled from "styled-components";
 
 const Container = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
   display: flex;
   flex-direction: column;
   justify-content: start;
   align-items: center;
-  min-height: 100vh; 
+  min-height: 100vh;
   width: 100vw;
   min-width: 768px;
-  background-color: white;
   color: black;
   z-index: 10;
 `;
-
 
 export const Main = () => {
   useEffect (() => {
@@ -24,16 +25,17 @@ export const Main = () => {
   }, []);
   return (
     <>
-      <Container>
+      <MainBackground>
+        <Container>
           <Header />
           {/* 퀴즈 리스트 컨트롤러 */}
           {/* TODO: 데이터를 불러오는 중에는 추가적인 처리 할 것 */}
           {/* TODO: 추가적으로 초기 상태에서 호출할 때 params를 넘겨서 ex)filter 인기순 설정 등 할 지 논의해볼 것 */}
           <ViewQuizList />
         </Container>
+      </MainBackground>
     </>
 
 
   );
 };
-
